ed827eb45c860104d10f9524aaa1f36ba8387607
Sam Korn <korn94sam@gmail.com>
2024-12-16 20:44:58 -0700

    buildroot needs to build

--- tools.sh
+++ tools.sh
@@ -1,4 +1,7 @@
 docker build -t foxbuilder:latest .
+
+export SDK_LOCAL_PATH=$(pwd)/luckfox-pico
+git clone https://github.com/LuckfoxTECH/luckfox-pico.git
 docker run -it --name luckfox-builder -v $(pwd):/mnt/host foxbuilder:latest
 
 export SDK_PATH=/mnt/host   
